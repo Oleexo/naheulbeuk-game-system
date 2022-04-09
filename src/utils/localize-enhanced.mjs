39e194ce0862a121392b3ee4d98cfd2881c8e681
Rafaël CASO <rafael.caso2@gmail.fr>
2022-04-09 15:45:41 +0200

    Add key composition to localize

--- src/utils/localize-enhanced.mjs
+++ src/utils/localize-enhanced.mjs
@@ -2,13 +2,26 @@ import logger from './logger';
 
 Hooks.once("ready", () => {
   game.i18n.localize = localize;
+  Handlebars.registerHelper('localize', localize);
   logger.log("Overriding game.i18n.localize()");
 });
 
-function localize(key) {
-  return tryGetLocalisation(key, game.i18n.translations)
+function localize(key, keyAddon = null, options = null) {
+  if (keyAddon != null && typeof keyAddon === 'string') {
+    key = `${key}.${keyAddon}`;
+  }
+  return tryFormat(key, keyAddon) || tryFormat(key, options)
+    || tryGetLocalisation(key, game.i18n.translations)
     || tryGetLocalisation(key, game.i18n._fallback)
     || key;
+}
+
+function tryFormat(key, options) {
+  if (options != null && typeof options === 'object') {
+    logger.log(`Trying to format ${key} with`, options, options.hash);
+    return game.i18n.format(key, options.hash);
+  }
+  return null;
 }
 
 function tryGetLocalisation(key, translations) {
